11ff88747c6426a056fb25c55eef4c4de7757625
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2018-09-23 09:53:29 +0200

    Commit 1

---
+++ src/Numbers.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Numbers {
+
+    public static void main(String[]args){
+        int[]numbers= new int[5];
+        numbers[0]=1;
+        numbers[1]=2;
+        numbers[2]=3;
+        numbers[3]=4;
+        numbers[4]=5;
+
+       System.out.println(numbers[0]+numbers[2]+numbers[4]);
+
+
+    }
+}
